ea9f264a5743c8bb056022796e4079bce80bd83c
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-10-22 10:56:55 +0100

    Feat: fixing responsiveness at smaller screen sizes

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -35,8 +35,8 @@ const DeleteBook = () => {
       <h1 className="text-3xl my-4">Delete Book</h1>
       {loading? (<Spinner />)
         : (
-          <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-            <h3 className="text-2xl">Are You Sure You want to delete this book?</h3>
+          <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl md:w-[600px] p-8 mx-auto">
+            <h3 className="text-2xl text-center">Are You Sure You want to delete this book?</h3>
               <button className="p-4 bg-red-600 text-white w-full m-8" onClick={handleDeleteBook}>Yes, Delete It</button>
           </div>)}
     </div>
