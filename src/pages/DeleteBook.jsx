ef05fb81bda8074a2bcaed10f0c3b7215c94d389
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-10-22 10:02:50 +0100

    Feat: changing link to deployed server

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-    .delete(`http://localhost:5555/books/${id}`)
+    .delete(`https://book-server-uc14.onrender.com/books/${id}`)
     .then(() => {
         setLoading(false);
         enqueueSnackbar("Book successfully deleted", {variant: 'success'})
