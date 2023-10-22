5c1b0d4ac794286e63689ee8d5f5149e72beb904
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-10-22 09:56:58 +0100

    Feat: fixing link to server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get("http://localhost:5555/books")
+    axios.get("https://book-server-uc14.onrender.com/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
